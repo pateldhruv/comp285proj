41dfa331a5a5a45033ef2df9648ba2ac3f9599f8
Mike Spallino <spallinom@wit.edu>
2014-11-02 22:41:24 -0500

    moved methods around.

--- base/ClientHandler.java
+++ base/ClientHandler.java
@@ -1,11 +1,27 @@
 package base;
 
+import java.util.Date;
+
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
 public abstract class ClientHandler extends ChannelInboundHandlerAdapter {
 
-	public abstract void channelRead(ChannelHandlerContext ctx, Object msg);
-	public abstract void exceptionCaught(ChannelHandlerContext ctx, Throwable cause);
+	public void channelRead(ChannelHandlerContext ctx, Object msg) {
+		ByteBuf m = (ByteBuf) msg;
+        try {
+            long currentTimeMillis = (m.readUnsignedInt() - 2208988800L) * 1000L;
+            System.out.println(new Date(currentTimeMillis));
+            ctx.close();
+        } finally {
+            m.release();
+        }
+	}
+	
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
 	
 }
--- chatroom/ChatroomClientHandler.java
+++ chatroom/ChatroomClientHandler.java
@@ -1,28 +1,7 @@
 package chatroom;
 
-import io.netty.buffer.ByteBuf;
-import io.netty.channel.ChannelHandlerContext;
-
-import java.util.Date;
-
 import base.ClientHandler;
 
 public class ChatroomClientHandler extends ClientHandler {
-    @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) {
-        ByteBuf m = (ByteBuf) msg;
-        try {
-            long currentTimeMillis = (m.readUnsignedInt() - 2208988800L) * 1000L;
-            System.out.println(new Date(currentTimeMillis));
-            ctx.close();
-        } finally {
-            m.release();
-        }
-    }
 
-    @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-        cause.printStackTrace();
-        ctx.close();
-    }
 }
